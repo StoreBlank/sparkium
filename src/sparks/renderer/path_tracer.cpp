f36fd999d4b8017bbf434f3d19dcc32ca3c535a6
伍垟圳 <wuyangch21@mails.tsinghua.edu.cn>
2024-01-15 15:13:16 +0800

    mis-glossy

--- src/sparks/renderer/path_tracer.cpp
+++ src/sparks/renderer/path_tracer.cpp
@@ -3,7 +3,10 @@
 #include "sparks/util/util.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/random.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <cmath>
+#include <iostream>
 #include <algorithm>
 
 namespace sparks {
@@ -24,6 +27,75 @@ bool PathTracer::RR(double p) const{
     return false;
 }
 
+PathTracer::SampledResult PathTracer::UniformSampling(glm::vec3 axis) const {
+  const double pi = 3.1415926;
+  glm::vec3 next_ray = glm::ballRand(1.0f);
+  if (glm::dot(next_ray, axis) < 0)
+    next_ray = -next_ray;
+  SampledResult sample;
+  sample.ray = next_ray;
+  sample.pdf = 1.0 / (2.0 * pi);
+  return sample;
+}
+PathTracer::SampledResult PathTracer::ConeSampling(glm::vec3 axis) const {
+  const double pi = 3.1415926;
+  glm::vec3 next_ray = glm::ballRand(1.0f);
+  while (glm::dot(next_ray, axis) < 0.99) {
+    next_ray = glm::ballRand(1.0f);
+  }
+  SampledResult sample;
+  sample.ray = next_ray;
+  sample.pdf = 1.0 / (0.02 * pi);
+  return sample;
+}
+
+PathTracer::SampledResult PathTracer::CosWeightedSampling(glm::vec3 axis, float exp) const {
+  const double pi = 3.1415926;
+  std::default_random_engine e(time(NULL));
+  std::uniform_real_distribution<double> u1(0, 1);
+  std::uniform_real_distribution<double> u2(0, 1);
+  float rnd1 = u1(e), rnd2 = u2(e);
+  float phi = 2 * pi * rnd2;
+  float theta = acos(pow(1 - rnd1, 1.0f / exp));
+  glm::vec3 sup(axis.y, axis.z, axis.x);
+  glm::vec3 vert = glm::normalize(glm::cross(axis, sup));
+
+  glm::vec3 phiaxis = axis + sin(theta) / cos(theta) * vert;
+  phiaxis = glm::normalize(phiaxis);
+  glm::vec3 sampled = phiaxis * cos(phi) +
+                      glm::cross(axis, phiaxis) * sin(phi) +
+                      axis * glm::dot(axis, phiaxis) * (1 - cos(phi));
+  SampledResult sample;
+  sample.pdf = sin(theta) * pow(cos(theta), exp) * (exp + 1) / 2.0f / pi;
+  sample.ray = glm::normalize(sampled);
+  return sample;
+}
+
+PathTracer::SampledResult PathTracer::BRDFWeightedSampling(
+    glm::vec3 axis,glm::vec3 wo, float alpha ) const {
+  const double pi = 3.1415926;
+  const float m_e = 2.7182818;
+  std::default_random_engine e(time(NULL));
+  std::uniform_real_distribution<double> u1(0, 1);
+  std::uniform_real_distribution<double> u2(0, 1);
+  float rnd1 = u1(e), rnd2 = u2(e);
+  float phi = 2 * pi * rnd2;
+  float theta = atan(sqrt(-alpha * alpha * log(1 - rnd1)));
+  glm::vec3 sup(axis.y, axis.z, axis.x);
+  glm::vec3 vert = glm::normalize(glm::cross(axis, sup));
+
+  glm::vec3 phiaxis = axis + sin(theta) / cos(theta) * vert;
+  phiaxis = glm::normalize(phiaxis);
+  glm::vec3 wh = phiaxis * cos(phi) +
+                      glm::cross(axis, phiaxis) * sin(phi) +
+                      axis * glm::dot(axis, phiaxis) * (1 - cos(phi));
+  glm::vec3 wi = 2.0f * glm::dot(wh,wo) * wh - wo;
+  SampledResult sample;
+  sample.pdf = 2 * sin(theta) / (pow(alpha,2) * pow(cos(theta), 3)) * pow(m_e, pow(tan(theta),2) / pow(alpha, 2)) / (4 * glm::dot(wo, wh));
+  sample.ray = glm::normalize(wi);
+  return sample;
+}
+
 float PathTracer::Phong_spec(glm::vec3 v1, glm::vec3 v2, glm::vec3 normal, float exp) const{
   glm::vec3 bivec = glm::normalize(v1 + v2);
   float cos_bn = glm::dot(bivec, normal);
@@ -40,11 +112,12 @@ glm::vec3 PathTracer::Shade(HitRecord intersection, glm::vec3 wo, int depth) con
       material.albedo_color *
       glm::vec3{scene_->GetTextures()[material.albedo_texture_id].Sample(
           intersection.tex_coord)} / pi;
-  auto brdf_s = 
+  auto brdf_s =
       material.specular_color *
       glm::vec3{scene_->GetTextures()[material.albedo_texture_id].Sample(
           intersection.tex_coord)} / pi;
   glm::vec3 L_dir(0, 0, 0), L_ind(0, 0, 0);
+  float sum_pdf = 0;
 
   // light source
   if (material.material_type == MATERIAL_TYPE_EMISSION) {
@@ -62,6 +135,7 @@ glm::vec3 PathTracer::Shade(HitRecord intersection, glm::vec3 wo, int depth) con
     auto &object = scene_->GetEntity(i);
     auto &material_i = scene_->GetEntity(i).GetMaterial();
     if (material_i.material_type == MATERIAL_TYPE_EMISSION) {
+      glm::vec3 L_this(0.0, 0.0, 0.0);
       double total_area = 0;
       glm::vec3 xl, vn;
       const Mesh *mesh_ptr = reinterpret_cast<const Mesh *>(object.GetModel());
@@ -109,19 +183,22 @@ glm::vec3 PathTracer::Shade(HitRecord intersection, glm::vec3 wo, int depth) con
       float dist2 = dist * dist;
       glm::vec3 intensity =
           material_i.emission * material_i.emission_strength * glm::vec3{scene_->GetTextures()[material_i.albedo_texture_id].Sample(hit_light.tex_coord)};
-      if (dots > 0 && (material.material_type == MATERIAL_TYPE_GLOSSY || material.material_type == MATERIAL_TYPE_LAMBERTIAN)) {
+      if (dots > 0) {
         float phong_spe = Phong_spec(wi,wo,intersection.geometry_normal, material.specular_exponent);
-        if (material.material_type != MATERIAL_TYPE_SPECULAR)
-          L_dir += alpha * intensity * brdf_d * cos_theta * cos_theta_hat / dist2 / pdf_light * visible;
-        if (material.material_type == MATERIAL_TYPE_GLOSSY ||
-            material.material_type == MATERIAL_TYPE_SPECULAR)
-          L_dir += alpha * intensity * brdf_s * phong_spe  * cos_theta_hat / dist2 /
+        if (material.material_type == MATERIAL_TYPE_LAMBERTIAN || material.material_type == MATERIAL_TYPE_GLOSSY)
+          L_this += alpha * intensity * brdf_d * cos_theta * cos_theta_hat / dist2 / pdf_light * visible;
+        if (material.material_type == MATERIAL_TYPE_GLOSSY)
+          L_this += alpha * intensity * brdf_s * phong_spe  * cos_theta_hat / dist2 /
                    pdf_light * visible;
+        if (material.material_type == MATERIAL_TYPE_TRANSMISSIVE)
+          L_this += alpha * intensity * brdf_d * cos_theta * cos_theta_hat /
+                   dist2 / pdf_light * visible;
       }
       if (dots < 0 && material.material_type == MATERIAL_TYPE_TRANSMISSIVE) {
-        L_dir += (1 - alpha) * intensity * brdf_d * cos_theta * cos_theta_hat /
+        L_this += (1 - alpha) * intensity * brdf_d * cos_theta * cos_theta_hat /
                  dist2 / pdf_light * visible;
       }
+      L_dir += L_this;
     }
   }
 
@@ -135,8 +212,7 @@ glm::vec3 PathTracer::Shade(HitRecord intersection, glm::vec3 wo, int depth) con
          2.0f * pi * scene_->GetEnvmapMajorColor() *
         std::max(glm::dot(light_direction, intersection.geometry_normal),
                  0.0f) * brdf_d;
-    if (material.material_type == MATERIAL_TYPE_GLOSSY ||
-        material.material_type == MATERIAL_TYPE_SPECULAR) {
+    if (material.material_type == MATERIAL_TYPE_GLOSSY) {
       
       L_dir += 2.0f * pi * scene_->GetEnvmapMajorColor() *
                brdf_s *
@@ -148,58 +224,92 @@ glm::vec3 PathTracer::Shade(HitRecord intersection, glm::vec3 wo, int depth) con
   // indirect light
   float p_rr = 0.6;
   if (RR(p_rr) && depth <= 8) {
-    glm::vec3 next_ray = glm::ballRand(1.0f);
-    if (glm::dot(next_ray, intersection.geometry_normal) < 0)
-      next_ray = -next_ray;
+    SampledResult sampleray ;
+    SampledResult sampleray2;
+    float mis_sum = 0.0f;
+    glm::vec3 reflect = glm::normalize(2.0f * glm::dot(intersection.geometry_normal, wo) *
+                            intersection.geometry_normal - wo);
+    if (material.material_type == MATERIAL_TYPE_GLOSSY) {
+      //sampleray2 =
+          //CosWeightedSampling(2.0f * glm::dot(intersection.geometry_normal, wo) * intersection.geometry_normal - wo, 400.0f);      
+      sampleray2 = UniformSampling(intersection.geometry_normal);
+      sampleray = CosWeightedSampling(reflect, 400.0f);
+
+    }
+    if (material.material_type == MATERIAL_TYPE_LAMBERTIAN) {
+      sampleray = UniformSampling(intersection.geometry_normal);
+    }
+    if (material.material_type == MATERIAL_TYPE_TRANSMISSIVE) {
+      std::default_random_engine e(time(NULL));
+      std::uniform_real_distribution<double> u(0, 1);
+      if (u(e) < alpha)
+        sampleray = UniformSampling(intersection.geometry_normal);
+      else
+        sampleray = CosWeightedSampling(-intersection.geometry_normal, 1.0f);
+    }
+    if (material.material_type == MATERIAL_TYPE_SPECULAR) {
+      sampleray.ray = reflect;
+      sampleray.pdf = 1.0;
+    }
+    glm::vec3 next_ray = sampleray.ray;
+    float pdf = sampleray.pdf;
     HitRecord next_intersection;
     auto t_n = scene_->TraceRay(intersection.position, next_ray, 1e-3f, 1e4f,
                               &next_intersection);
-    if (t_n > 0 && scene_->GetEntity(next_intersection.hit_entity_id)
-                           .GetMaterial()
-                           .material_type != MATERIAL_TYPE_EMISSION) {
-      glm::vec3 intensity = Shade(next_intersection, -next_ray, depth+1) / p_rr;
-      if (material.material_type != MATERIAL_TYPE_SPECULAR)
-        L_ind += alpha * 2.0f * pi * brdf_d * intensity *
-               abs(glm::dot(intersection.geometry_normal, next_ray));
-      if (material.material_type == MATERIAL_TYPE_GLOSSY ||
-          material.material_type == MATERIAL_TYPE_SPECULAR) {
-        L_ind +=
-            alpha * 2.0f * pi * intensity * brdf_s *
-            Phong_spec(next_ray, wo, intersection.geometry_normal,
-                       material.specular_exponent);
-      }
+    if (t_n > 0) {
+      auto hitmaterial_type = scene_->GetEntity(next_intersection.hit_entity_id)
+                                  .GetMaterial()
+                                  .material_type;
+      glm::vec3 intensity =
+          Shade(next_intersection, -next_ray, depth + 1) / p_rr;
+      if (material.material_type != MATERIAL_TYPE_SPECULAR &&
+          hitmaterial_type != MATERIAL_TYPE_EMISSION) {
+          L_ind += brdf_d * intensity *
+                   abs(glm::dot(intersection.geometry_normal, next_ray)) / pdf;
+    }
+      if (material.material_type == MATERIAL_TYPE_SPECULAR)
+        L_ind += intensity;
+      if (material.material_type == MATERIAL_TYPE_GLOSSY)
+        L_ind += intensity * brdf_s *
+                 Phong_spec(next_ray, wo, intersection.geometry_normal,
+                            material.specular_exponent) /
+                 float(sampleray.pdf);
 
     } else if (t_n < 0) {
       if (material.material_type != MATERIAL_TYPE_SPECULAR)
-        L_ind += alpha * 2.0f * pi * brdf_d * glm::vec3{scene_->SampleEnvmap(next_ray)} *
-               abs(glm::dot(intersection.geometry_normal, next_ray));
-      if (material.material_type == MATERIAL_TYPE_GLOSSY ||
-          material.material_type == MATERIAL_TYPE_SPECULAR) {
-        L_ind += alpha * 2.0f * pi * brdf_s *
-                 glm::vec3{scene_->SampleEnvmap(next_ray)} *
+        L_ind += brdf_d * glm::vec3{scene_->SampleEnvmap(next_ray)} *
+               abs(glm::dot(intersection.geometry_normal, next_ray)) / pdf;
+      if (material.material_type == MATERIAL_TYPE_SPECULAR)
+        L_ind += glm::vec3{scene_->SampleEnvmap(next_ray)};
+      if (material.material_type == MATERIAL_TYPE_GLOSSY) {
+        L_ind += brdf_s * glm::vec3{scene_->SampleEnvmap(sampleray.ray)} *
                  Phong_spec(next_ray, wo, intersection.geometry_normal,
-                            material.specular_exponent);
+                            material.specular_exponent) /
+                 float(sampleray.pdf);
       }
     }
-    if (material.material_type == MATERIAL_TYPE_TRANSMISSIVE) {
-      next_ray = -next_ray;
-      HitRecord next_intersection;
-      auto t_n = scene_->TraceRay(intersection.position, next_ray, 1e-3f, 1e4f,
+    if (material.material_type == MATERIAL_TYPE_GLOSSY) {
+      mis_sum += pdf;
+      L_ind *= pdf;
+      auto t_n = scene_->TraceRay(intersection.position, sampleray2.ray, 1e-3f, 1e4f,
                                   &next_intersection);
-      if (t_n > 0 && scene_->GetEntity(next_intersection.hit_entity_id)
-                             .GetMaterial()
-                             .material_type != MATERIAL_TYPE_EMISSION) {
+      if (t_n > 0) {
         glm::vec3 intensity =
-            Shade(next_intersection, -next_ray, depth + 1) / p_rr;
-        L_ind += (1 - alpha) * 2.0f * pi * brdf_d * intensity *
-                   abs(glm::dot(intersection.geometry_normal, next_ray));
-
-      } else if (t_n < 0) {
-        L_ind += (1 - alpha) * 2.0f * pi * brdf_d *
-                   glm::vec3{scene_->SampleEnvmap(next_ray)} *
-                   abs(glm::dot(intersection.geometry_normal, next_ray));
+            Shade(next_intersection, -sampleray2.ray, depth + 1) / p_rr;
+          L_ind += intensity * brdf_s *
+                   Phong_spec(sampleray2.ray, wo, intersection.geometry_normal,
+                              material.specular_exponent);
+          L_ind += brdf_d * intensity *
+                   abs(glm::dot(intersection.geometry_normal, sampleray2.ray));
+      } else {
+        L_ind += brdf_d * glm::vec3{scene_->SampleEnvmap(sampleray2.ray)} *
+                 abs(glm::dot(intersection.geometry_normal, sampleray2.ray));
+        L_ind += brdf_s * glm::vec3{scene_->SampleEnvmap(sampleray2.ray)} *
+                 Phong_spec(sampleray2.ray, wo, intersection.geometry_normal,
+                            material.specular_exponent);
       }
-
+      mis_sum += sampleray2.pdf;
+      L_ind /= mis_sum;
     }
         
   }
--- src/sparks/renderer/path_tracer.h
+++ src/sparks/renderer/path_tracer.h
@@ -14,6 +14,14 @@ class PathTracer {
                                     int sample) const;
   glm::vec3 Shade(HitRecord interseciton, glm::vec3 wo, int depth) const;
   bool RR(double p) const;
+  struct SampledResult {
+    glm::vec3 ray;
+    double pdf;
+  };
+  SampledResult PathTracer::UniformSampling(glm::vec3 axis) const;
+  SampledResult PathTracer::BRDFWeightedSampling(glm::vec3 axis, glm::vec3 wo, float alpha) const;
+  SampledResult PathTracer::CosWeightedSampling(glm::vec3 axis, float exp) const;
+  SampledResult PathTracer::ConeSampling(glm::vec3 axis) const;
   float PathTracer::Phong_spec(glm::vec3 v1,
                                glm::vec3 v2,
                                glm::vec3 normal,
